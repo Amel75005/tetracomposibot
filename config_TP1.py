416b93e883110bd494077c79f242a244c8814558
Amel <amel.ben.chabane22@gmail.com>
2026-01-29 16:42:22 +0100

    TP1 Exo1 Comportement5

--- config_TP1.py
+++ config_TP1.py
@@ -23,13 +23,13 @@ display_time_stats = True
 
 # initialization : create and place robots at initial positions (returns a list containing the robots)
 
-import robot_braitenberg_loveBot
+import robot_braitenberg_hateBot
 
 def initialize_robots(arena_size=-1, particle_box=-1): # particle_box: size of the robot enclosed in a square
     #x_center = arena_size // 2 - particle_box / 2
     y_center = arena_size // 2 - particle_box / 2
     robots = []
-    robots.append(robot_braitenberg_loveBot.Robot_player(10, y_center, 0, name="My Robot", team="A"))
-    robots.append(robot_dumb.Robot_player(13, y_center + 2, 180, name="Amel", team="B"))
-    robots.append(robot_dumb.Robot_player(20, y_center - 2, 180, name="Tassa", team="B"))
+    robots.append(robot_braitenberg_hateBot.Robot_player(20, y_center, 0, name="My Robot", team="A"))
+    robots.append(robot_dumb.Robot_player(23, y_center + 2, 180, name="Amel", team="B"))
+    robots.append(robot_dumb.Robot_player(30, y_center - 2, 180, name="Tassa", team="B"))
     return robots
